f6bf0bac7549563512c1beaf706a7588d7e9ff70
Aaron Ftv <aaronfuentevilla@gmail.com>
2024-06-05 03:09:38 -0500

    Adding project structure, data entities, DB config

--- src/main/java/com/aaron/merch_aaron/web/controller/OlaKeAseController.java
+++ src/main/java/com/aaron/merch_aaron/web/controller/OlaKeAseController.java
@@ -1,4 +1,4 @@
-package com.aaron.merch_aaron;
+package com.aaron.merch_aaron.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
